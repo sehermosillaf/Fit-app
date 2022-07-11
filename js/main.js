e7c3958964210cd183143d270fff795dbb21bbba
Sebastian Hermosilla <se.hermosillaf@gmail.com>
2022-07-11 12:56:07 -0400

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+//gender form
+const male = document.getElementById("male");
+const outputBox = document.getElementById('output-box');
+const submitBtn = document.getElementById('submit');
+
+
+
+const output = document.getElementById();
+
